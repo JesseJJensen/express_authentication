d58ed144b03abd2eb347b58816a3bde297cca623
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-14 10:43:41 -0400

    alerts: add partials for flash alerts

--- test/profile.test.js
+++ test/profile.test.js
@@ -13,7 +13,7 @@ before(function(done) {
 describe('GET /profile', function() {
   it('should redirect to /auth/login if not logged in', function(done) {
     request(app).get('/profile')
-    .expect('Location', '/auth/login')
+    .expect('Location', './auth/login')
     .expect(302, done);
   });
 

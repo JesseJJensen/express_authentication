41a101c23b777f006ea1ae85667193b5bf0c2929
Jesse Jensen <81991370+JesseJJensen@users.noreply.github.com>
2021-05-14 12:01:24 -0400

    done

--- server.js
+++ server.js
@@ -20,12 +20,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
 
+
 app.use(session({
   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
   resave: false,             // Save the session even if it's modified, make this false
   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
 }));
 app.use(flash());            // flash middleware
+
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
 
 app.use((req, res, next) => {
   console.log(res.locals);
@@ -34,19 +38,22 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(passport.initialize());      // Initialize passport
-app.use(passport.session());         // Add a session
 
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/profile', (req, res) => {
-  res.render('profile');
-});
+// app.get('/profile', (req, res) => {
+//   res.render('profile');
+// });
 
 app.use('/auth', require('./controllers/auth'));
+
+app.get('/profile', isLoggedIn, (req, res) => {
+  const { id, name, email } = req.user.get(); 
+  res.render('profile', { id, name, email });
+});
 
 
 const PORT = process.env.PORT || 3000;
